b4b55cbb8c2b6c41562ec6a823ed8922623654ff
liyi <760862629@qq.com>
2021-05-09 21:56:56 +0800

    Desgraption: 提交测试代码A Date: 2021年05月09日20:42:32 Author: lyi

--- src/main/java/com/example/springinitializr/juc/hanxunping/CpuNum.java
+++ src/main/java/com/example/springinitializr/juc/hanxunping/CpuNum.java
@@ -12,6 +12,7 @@ public class CpuNum {
         //获取当前电脑的cpu数量/核心数
         int cpuNums = runtime.availableProcessors();
         System.out.println("当前有cpu 个数=" + cpuNums);
+        System.out.println("测试提交A");
 
 
     }
