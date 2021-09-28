93d22e0f75bf104772a975afc65943fb24faffb1
liyi <18684997125@163.com>
2021-09-28 22:48:59 +0800

    Desgraption: 1. HM-JUC实战04

--- src/main/java/com/example/springinitializr/juc/HM/demo/buis/StampedThread.java
+++ src/main/java/com/example/springinitializr/juc/HM/demo/buis/StampedThread.java
@@ -24,23 +24,23 @@ public class StampedThread {
 
     public static void main(String[] args) {
         StampedThread stampedThread = new StampedThread();
-
         Thread t1 = new Thread(()->{
             while (true) {
                 stampedThread.write();
             }
         });
-
         Thread t2 = new Thread(()->{
             while (true){
                 stampedThread.read();
             }
         });
-
         t1.setPriority(9);
         t2.setPriority(1);
-
         t1.start();
         t2.start();
+        /**
+         * 结果分析： read间隔性打出，提升了读操作的并发性 注意，StampedLock的使用有局限性！ 对于读多写少的场景 StampedLock 性能很好，简单的应用场景基本上可以替代 ReadWriteLock
+         * StampedLock 在命名上并没有 Reentrant，StampedLock 是不可重入的！ StampedLock 的悲观读锁、写锁都不支持条件变量（无法使用Condition）
+         */
     }
 }
